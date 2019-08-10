2b175dd2ec8ca4660b21ec78fa054c94b93ffb87
Stoneburner Software <ssfw@Seans-MacBook-Pro.local>
2019-08-10 08:17:53 -0400

    signup form and handleChange

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../core/Layout'
+import {API} from '../config'
 
-const Signup = () => (
-  <Layout title="Start your ecommerce store" description="Sign up">
-    ...
-  </Layout>
-);
+const Signup = () => {
+  const [values, setValues] = useState({
+    name: '',
+    email: '',
+    password: '',
+    error: '',
+    success: false
+  })
+
+  const handleChange = name => event => {
+    setValues({...values, error: false, [name]: event.target.value})
+  }
+
+  const signUpForm = () => (
+    <form>
+      <div className="form-group">
+        <label className="text-muted">Name</label>
+        <input onChange={handleChange('name')} type="text" className="form-control"></input>
+      </div>
+      <div className="form-group">
+        <label className="text-muted">Email</label>
+        <input onChange={handleChange('email')} type="email" className="form-control"></input>
+      </div>
+      <div className="form-group">
+        <label className="text-muted">Password</label>
+        <input onChange={handleChange('password')} type="password" className="form-control"></input>
+      </div>
+      <button className="btn btn-primary">Submit</button>
+    </form>
+  )
+  return (
+    <Layout 
+      title="Start your ecommerce store" 
+      description="Sign up"
+      className="container col-md-8 offset-md-2"
+    >
+      { signUpForm() }
+      {JSON.stringify(values)}
+    </Layout>
+  );
+};
 
 export default Signup;
