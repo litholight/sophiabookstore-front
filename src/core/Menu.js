439b4bc1aa8cf53d8aa7671403f905bf9f761533
Stoneburner Software <ssfw@Seans-MacBook-Pro.local>
2019-08-11 13:27:22 -0400

    user signout

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
+import {signout} from '../auth'
 
 const isActive = (history, path) => {
   if(history.location.pathname === path) {
@@ -20,6 +21,11 @@ const Menu = ({history}) => (
       </li>
       <li className="nav-item">
         <Link className="nav-link" to="/signup" style={isActive(history, '/signup')}>Signup</Link>
+      </li>
+      <li className="nav-item">
+        <span className="nav-link" onClick={() => signout(() => {
+          history.push("/");
+        })} style={{curser: 'pointer', color: '#ffffff'}}>Signout</span>
       </li>
     </ul>
   </div>
