fa0bd3be7847aa2d5cd556f321517c78bf7ef9b4
Stoneburner Software <ssfw@Seans-MacBook-Pro.local>
2019-08-11 08:59:36 -0400

    Back to refactored signup with local env

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import Layout from '../core/Layout'
-import {API} from '../config'
+import {Link} from 'react-router-dom';
+import Layout from '../core/Layout';
+import {signup} from '../auth';
 
 const Signup = () => {
   const [values, setValues] = useState({
@@ -11,60 +12,86 @@ const Signup = () => {
     success: false
   });
 
-  const {name, email, password} = values
+  const {name, email, password, success, error} = values
 
   const handleChange = name => event => {
     setValues({...values, error: false, [name]: event.target.value})
   }
 
-  const signup = (user) => {
-    // console.log(name, email, password)
-    fetch(`${API}/signup`, {
-      method: "POST",
-      headers: {
-        Accept: 'application/json',
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(user)
-    })
-    .then(response => {
-      return response.json()
-    })
-    .catch(err => {
-      console.log(err)
-    })
-  }
-
   const clickSubmit = (event) => {
      event.preventDefault();
+     setValues({ ...values, error: false })
      signup({name, email, password})
+     .then(data => {
+       if(data.error) {
+         setValues({...values, error: data.error, success: false})
+       } else {
+         setValues({
+           ...values,
+           name: '',
+           email: '',
+           password: '',
+           error: '',
+           success: true
+         })
+       }
+     })
   }
 
   const signUpForm = () => (
     <form>
       <div className="form-group">
         <label className="text-muted">Name</label>
-        <input onChange={handleChange('name')} type="text" className="form-control"></input>
+        <input 
+          onChange={handleChange('name')} 
+          type="text" 
+          className="form-control"
+          value={name}
+        ></input>
       </div>
       <div className="form-group">
         <label className="text-muted">Email</label>
-        <input onChange={handleChange('email')} type="email" className="form-control"></input>
+        <input 
+          onChange={handleChange('email')} 
+            type="email" 
+            className="form-control"
+            value={email}
+          ></input>
       </div>
       <div className="form-group">
         <label className="text-muted">Password</label>
-        <input onChange={handleChange('password')} type="password" className="form-control"></input>
+        <input 
+          onChange={handleChange('password')} 
+          type="password" className="form-control"
+          value={password}
+        ></input>
       </div>
       <button onClick={clickSubmit} className="btn btn-primary">Submit</button>
     </form>
   )
+
+  const showError = () => (
+    <div className="alert alert-danger" style={{display: error ? '' : 'none'}}>
+      {error}
+    </div>
+  )
+
+  const showSuccess = () => (
+    <div className="alert alert-danger" style={{display: success ? '' : 'none'}}>
+      New account is created.  Please <Link to="/signin">Sign in</Link>
+    </div>
+  );
+
+
   return (
     <Layout 
       title="Start your ecommerce store" 
       description="Sign up"
       className="container col-md-8 offset-md-2"
     >
+      {showSuccess()}
+      {showError()}
       { signUpForm() }
-      {JSON.stringify(values)}
     </Layout>
   );
 };
