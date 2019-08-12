af3276a8b2c8edb2d96aef1d9f5b05e215e510c7
Stoneburner Software <ssfw@Seans-MacBook-Pro.local>
2019-08-12 17:54:25 -0400

    form for create product

--- src/admin/AddProduct.js
+++ src/admin/AddProduct.js
@@ -7,12 +7,102 @@ import {createProduct} from "./apiAdmin";
 const AddProduct = () => {
 
   const {user, token} = isAuthenticated();
+  const [values, setValues] = useState({
+    name: '',
+    description: '',
+    price: '',
+    categories: [],
+    category: '',
+    quantity: '',
+    photo: '',
+    loading: false,
+    error: '',
+    createdProduct: '',
+    redirectToProfile: false,
+    formData: ''
+  })
+
+  const {
+    name,
+    description,
+    price,
+    categories,
+    category,
+    quantity,
+    loading,
+    error,
+    createdProduct,
+    redirectToProfile,
+    formData
+  } = values;
+
+  useEffect(() => {
+    setValues({...values, formData: new FormData()})
+  }, [])
+
+  const handleChange = name => event => {
+    const value = name === 'photo' ? event.target.files[0]: event.target.value;
+    formData.set(name, value)
+    setValues({...values, [name]: value})
+  };
+
+  const clickSubmit = (event) => {
+    //
+  }
+
+  const newPostForm = () => (
+    <form className="mb-3" onSubmit={clickSubmit}>
+      <h4>Post photo</h4>
+      <div className="form-group">
+        <label className="btn btn-outline-secondary">
+        <input onChange={handleChange('photo')} type="file" name="photo" accept="image/*"/>
+        </label>
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Name</label>
+        <input onChange={handleChange('name')} type="text" className="form-control" value={name}/>
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Description</label>
+        <textarea onChange={handleChange('description')}className="form-control" value={description}/>
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Price</label>
+        <input onChange={handleChange('price')} type="number" className="form-control" value={price}/>
+      </div>
+
+      <div className="form-group">
+          <label className="text-muted">Category</label>
+          <select onChange={handleChange('category')} className="form-control">
+            <option value="5d5156ccb9e17b21833cbd9a">Philosophy of Science</option>
+          </select>
+      </div>
+
+      <div className="form-group">
+          <label className="text-muted">Shipping</label>
+          <select onChange={handleChange('shipping')} className="form-control">
+            <option value="0">No</option>
+            <option value="1">Yes</option>
+          </select>
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Quantity</label>
+        <input onChange={handleChange('quantity')} type="number" className="form-control" value={quantity}/>
+      </div>
+      <button className="btn btn-outline-primary">Create Product</button>
+
+    </form>
+  );
 
   return (
     <Layout title="Add a new product" description={`Hello, ${user.name}! `}>
       <div className="row">
         <div className="col-md-8 offset-md-2">
-          ...
+          {newPostForm()}
         </div>
       </div>
     </Layout>
