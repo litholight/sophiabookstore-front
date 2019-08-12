31207c226d310fba1d917c404e8f1f71cf6f465a
Stoneburner Software <ssfw@Seans-MacBook-Pro.local>
2019-08-11 22:14:25 -0400

    created adminroute and setup redirect

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -17,9 +17,17 @@ const Menu = ({history}) => (
         <Link className="nav-link" to="/" style={isActive(history, '/')}>Home</Link>
       </li>
 
-      <li className="nav-item">
-        <Link className="nav-link" to="/user/dashboard" style={isActive(history, '/user/dashboard')}>Dashboard</Link>
-      </li>
+      {isAuthenticated() && isAuthenticated().user.role === 0 && (
+        <li className="nav-item">
+          <Link className="nav-link" to="/user/dashboard" style={isActive(history, '/user/dashboard')}>Dashboard</Link>
+        </li>
+      )}
+
+      {isAuthenticated() && isAuthenticated().user.role === 1 && (
+        <li className="nav-item">
+          <Link className="nav-link" to="/admin/dashboard" style={isActive(history, '/admin/dashboard')}>Dashboard</Link>
+        </li>
+      )}
       
       {!isAuthenticated() && (
         <Fragment>
