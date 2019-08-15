8abd0dccb95ee7c8239552b79e167253703cd79f
Stoneburner Software <ssfw@Seans-MacBook-Pro.local>
2019-08-15 15:35:01 -0400

    handle categories toggle

--- src/core/Shop.js
+++ src/core/Shop.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import Layout from './Layout'
 import Card from './Card';
 import {getCategories} from './apiCore';
+import Checkbox from './Checkbox'
 
 const Shop = () => {
   const [categories, setCategories] = useState([])
@@ -23,10 +24,13 @@ const Shop = () => {
   }, [])
 
   return (
-    <Layout title="Sophia's Bookstore" description="Used books at a great price." className="container-fluid">
+    <Layout title="Sophia's Bookstore" description="Used books at a great price!" className="container-fluid">
       <div className="row">
         <div className="col-4">
-          {JSON.stringify(categories)}
+          <h4>Filter by categories</h4>
+          <ul>
+            <Checkbox categories={categories}/>
+          </ul>
         </div>
         <div className="col-8">right</div>
       </div>
