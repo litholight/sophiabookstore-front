449aa7138261031c3e90c14ad5e5f219d4b04690
Stoneburner Software <sean@stoneburnersoftware.com>
2019-08-18 09:32:52 -0400

    added categories from backend to search

--- src/core/Home.js
+++ src/core/Home.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import Layout from './Layout'
 import {getProducts} from './apiCore';
 import Card from './Card';
+import Search from './Search';
 
 
 const Home = () => {
@@ -36,6 +37,8 @@ const Home = () => {
 
   return (
     <Layout title="Sophia's Bookstore" description="A live demonstration of Stoneburner Software" className="container-fluid">
+
+      <Search />
       
       <h2 className="mb-4">New Arrivals</h2>
       <div className="row">
--- src/core/Search.js
+++ src/core/Search.js
@@ -1,8 +1,37 @@
 import React, {useState, useEffect} from 'react';
-import Layout from './Layout'
-import {getProducts} from './apiCore';
+import {getCategories} from './apiCore';
 import Card from './Card';
 
 const Search = () => {
+  const [data, setData] = useState({
+    categories: [],
+    category: '',
+    search: '',
+    results: [],
+    searched: false 
+  });
+
+  const {categories, category, search, results, searched} = data
   
-}
+  const loadCategories = () => {
+    getCategories().then(data => {
+      if(data.error) {
+        console.log(data.error)
+      } else {
+        setData({...data, categories: data})
+      }
+    })
+  };
+
+  useEffect(() => {
+    loadCategories()
+  }, [])
+
+  return (
+    <div>
+      <h2>Search bar {JSON.stringify(categories)}</h2>
+    </div>
+  );
+};
+
+export default Search; 
