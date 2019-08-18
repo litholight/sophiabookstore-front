423aceed5644fc7df209dcc1e1487a3e775e0270
Stoneburner Software <litholight@gmail.com>
2019-08-18 09:10:50 -0400

    created load more button

--- src/core/Shop.js
+++ src/core/Shop.js
@@ -14,7 +14,8 @@ const Shop = () => {
   const [error, setError] = useState(false)
   const [limit, setLimit] = useState(6)
   const [skip, setSkip] = useState(0)
-  const [filteredResults, setFilteredResults] = useState(0)
+  const [filteredResults, setFilteredResults] = useState([])
+  const [size, setSize] = useState(0)
 
    // load categories and set form data
    const init = () => {
@@ -33,9 +34,33 @@ const Shop = () => {
       if(data.error) {
         setError(data.error)
       } else {
-        setFilteredResults(data)
+        setFilteredResults(data.data);
+        setSize(data.size);
+        setSkip(0);
       }
     })
+  };
+
+  const loadMore = () => {
+    let toSkip = skip + limit
+    // console.log(newFilters)
+    getFilteredProducts(toSkip, limit, myFilters.filters).then(data => {
+      if(data.error) {
+        setError(data.error)
+      } else {
+        setFilteredResults([...filteredResults, ...data.data]);
+        setSize(data.size);
+        setSkip(toSkip);
+      }
+    })
+  };
+
+  const loadMoreButton = () => {
+    return (
+      size > 0 &&  size >= limit && (
+        <button onClick={loadMore} className="btn btn-warning mb-5">Load more</button>
+      )
+    );
   };
 
   useEffect(() => {
@@ -82,7 +107,16 @@ const Shop = () => {
             <RadioBox prices={prices} handleFilters={filters => handleFilters(filters, 'price')}/>
           </div>
         </div>
-        <div className="col-8">{JSON.stringify(filteredResults)}</div>
+        <div className="col-8">
+          <h2 className="mb-4">Products</h2>
+          <div className="row">
+            {filteredResults.map((product, i) => (
+                <Card key={i} product={product} />
+            ))}
+          </div>
+          <hr/>
+          {loadMoreButton()}
+        </div>
       </div>
     </Layout>
   )
