b4bfa88833bbbfc64b89ebcc18cdc2d6a957f4c0
Stoneburner Software <ssfw@Seans-MacBook-Pro.local>
2019-08-17 22:36:55 -0400

    added price filter, show products by filters

--- src/core/Shop.js
+++ src/core/Shop.js
@@ -1,12 +1,20 @@
 import React, {useState, useEffect} from 'react';
 import Layout from './Layout'
 import Card from './Card';
-import {getCategories} from './apiCore';
-import Checkbox from './Checkbox'
+import {getCategories, getFilteredProducts} from './apiCore';
+import Checkbox from './Checkbox';
+import {prices} from './fixedPrices';
+import RadioBox from './RadioBox';
 
 const Shop = () => {
+  const [myFilters, setMyFilters] = useState({
+    filters: {category: [], price: []}
+  })
   const [categories, setCategories] = useState([])
   const [error, setError] = useState(false)
+  const [limit, setLimit] = useState(6)
+  const [skip, setSkip] = useState(0)
+  const [filteredResults, setFilteredResults] = useState(0)
 
    // load categories and set form data
    const init = () => {
@@ -19,12 +27,45 @@ const Shop = () => {
     });
   };
 
+  const loadFilteredResults = (newFilters) => {
+    // console.log(newFilters)
+    getFilteredProducts(skip, limit, newFilters).then(data => {
+      if(data.error) {
+        setError(data.error)
+      } else {
+        setFilteredResults(data)
+      }
+    })
+  };
+
   useEffect(() => {
     init();
+    loadFilteredResults(skip, limit, myFilters.filters)
   }, []);
 
   const handleFilters = (filters, filterBy) => {
-    console.log("SHOP", filters, filterBy);
+    // console.log("SHOP", filters, filterBy);
+    const newFilters = {...myFilters};
+    newFilters.filters[filterBy] = filters;
+
+    if(filterBy == "price") {
+      let priceValues = handlePrice(filters);
+      newFilters.filters[filterBy] = priceValues;
+    }
+    loadFilteredResults(myFilters.filters);
+    setMyFilters(newFilters);
+  };
+
+  const handlePrice = value => {
+    const data = prices;
+    let array = []
+
+    for(let key in data) {
+      if(data[key]._id === parseInt(value)) {
+        array = data[key].array
+      }
+    }
+    return array;
   }
 
   return (
@@ -33,10 +74,15 @@ const Shop = () => {
         <div className="col-4">
           <h4>Filter by categories</h4>
           <ul>
-            <Checkbox categories={categories} handleFilters={filters => handleFilters(filters, 'category ')}/>
+            <Checkbox categories={categories} handleFilters={filters => handleFilters(filters, 'category')}/>
           </ul>
+
+          <h4>Filter by price</h4>
+          <div>
+            <RadioBox prices={prices} handleFilters={filters => handleFilters(filters, 'price')}/>
+          </div>
         </div>
-        <div className="col-8">right</div>
+        <div className="col-8">{JSON.stringify(filteredResults)}</div>
       </div>
     </Layout>
   )
